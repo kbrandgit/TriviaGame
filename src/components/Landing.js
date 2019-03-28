637a6682956d19d0b30cb10058f89af341651523
Ritchie Fesperman <ritchie.fes@gmail.com>
2019-03-27 22:05:53 -0400

    update boilerplate index.html, render Landing in index.js, build Landing component

---
+++ src/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+function Landing() {
+  return (
+    <div className="container-fluid landing-container">
+      <div className="row landing-top-row align-items-center">
+        <div className="col landing-col">PSc5 Trivia</div>
+      </div>
+      <div className="row landing-bottom-row align-items-center justify-content-center" />
+    </div>
+  );
+}
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './components/Landing';
+import './index.css';
 
 ReactDOM.render(
-      <div>
-        trivia game
-      </div>,
+  <div>
+    <Landing />
+  </div>,
   document.getElementById('root')
 );
