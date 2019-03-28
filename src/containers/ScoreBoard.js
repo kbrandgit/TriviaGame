cbf35154c4ec3a6ef3365f2b9574791f4560a5c9
Knox Brand <knox.brand@gmail.com>
2019-03-28 12:45:12 -0400

    added scoreboard and other game logic

---
+++ src/containers/ScoreBoard.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import { gameState } from "../actions";
+import { bindActionCreators } from "redux";
+
+class ScoreBoard extends Component {
+  
+  componentDidMount() {
+    this.props.gameState();
+  }
+
+  renderPlayers() {
+    return _.map(this.props.gameData.players, player => {
+      return (
+        <li className="players">
+        name: {player.name} score: {player.score}
+        </li>
+      )
+    })
+
+  }
+
+  render() {
+    console.log('scoreboard: ', this.props.gameData)
+    return (
+    <div>
+      <ul className="playersGroup">
+          {this.renderPlayers()}
+        </ul>
+    </div>
+    )
+  }
+}
+
+function mapStateToProps({gameData}) {
+  return {gameData}; 
+}
+
+function mapDispatchToProps(dispatch) { //allows our functions to be dispatched to the middleware then reducers when the functions are invokes
+  return bindActionCreators({gameState}, dispatch); //({action creators}, dispatch)
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ScoreBoard);
