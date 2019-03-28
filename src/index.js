5af4e2cf5c14aa0c6281eafc6c2c2d5b1ed81f3a
Ritchie Fesperman <ritchie.fes@gmail.com>
2019-03-28 12:27:43 -0400

    add LobbyLanding.js and link /landing and /lobby via button

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import promise from "redux-promise";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import reducers from "./reducers";
+import reducers from './reducers';
 import Landing from './components/Landing';
 import Gameroom from './containers/Gameroom';
-import Gameover from "./components/gameover";
+import Gameover from './components/gameover';
+import LobbyLanding from './components/LobbyLanding';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <BrowserRouter>
       <div>
         <Switch>
-          <Route path="/Landing" component={Landing} />
-          <Route path="/Gameroom" component={Gameroom} />
-          <Route path="/Gameover" component={Gameover} />
+          <Route path="/landing" component={Landing} />
+          <Route path="/gameroom" component={Gameroom} />
+          <Route path="/gameover" component={Gameover} />
+          <Route path="/lobby" component={LobbyLanding} />
         </Switch>
       </div>
     </BrowserRouter>
